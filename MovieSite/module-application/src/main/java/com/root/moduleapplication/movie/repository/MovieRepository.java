231b73f8b732af94fdc7f03d3137a42ca20ec8ff
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-03-23 13:23:47 +0900

    ADD 2주차 제목 검색과 개봉일 기준 반환

--- MovieSite/module-application/src/main/java/com/root/moduleapplication/movie/repository/MovieRepository.java
+++ MovieSite/module-application/src/main/java/com/root/moduleapplication/movie/repository/MovieRepository.java
@@ -3,6 +3,14 @@ package com.root.moduleapplication.movie.repository;
 import com.root.moduledomain.movie.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MovieRepository extends JpaRepository<Movie, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+    @Override
+    Optional<Movie> findById(Long aLong);
+
+//    2주차 구현 과제
+    Optional<List<Movie>> findAllByTitle(String title);
+    Optional<List<Movie>> findAllByReleaseDate(String releaseDate);
 }
